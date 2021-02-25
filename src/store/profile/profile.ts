06937c17f5f164184249b81852811f7f28b220e0
nargok <nargok@gmail.com>
2021-02-25 22:54:08 +0900

    wip: sign page

---
+++ src/store/profile/profile.ts
@@ -0,0 +1,41 @@
+import store from "@/store";
+import {
+  Module,
+  VuexModule,
+  getModule,
+  Mutation,
+  Action
+} from "vuex-module-decorators";
+import axios from "axios";
+
+const setStorage = (profile: Record<string, any>) => {
+  localStorage.setItem("profile", JSON.stringify(profile));
+};
+
+@Module({ dynamic: true, store, name: "profile", namespaced: true })
+class ProfileModule extends VuexModule {
+  // TODO 型定義
+  public profile: Record<string, any> | null = null;
+
+  @Mutation
+  private saveProfile(profile: Record<string, any>) {
+    this.profile = profile;
+    setStorage(profile);
+  }
+
+  @Action
+  public async signInAsync(email: string, password: string) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      const response = await axios.post("/signin", {
+        email,
+        password
+      });
+      this.saveProfile(response.data);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export const profileStore = getModule(ProfileModule);
---
+++ src/views/SignIn.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="box-card">
+    <h1>SignIn</h1>
+    <el-row type="flex" justify="center" class="signin-form">
+      <el-col :span="8">
+        <div style="margin-top: 15px;">
+          <el-input placeholder="email" v-model="email"></el-input>
+        </div>
+        <div style="margin-top: 15px;">
+          <el-input placeholder="password" v-model="password"></el-input>
+        </div>
+        <div style="margin-top: 15px; margin-bottom: 15px">
+          <el-button>サインイン</el-button>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    // HelloWorld
+  }
+})
+export default class SignIn extends Vue {
+  private email: string | null = null;
+  private password: string | null = null;
+}
+</script>
+
+<style scoped>
+.signin-form {
+  /* max-width: 980px; */
+  /* min-width: 100px; */
+  /* border: 1px solid #ececec; */
+}
+</style>
